70e8992025b240b7be61e178078988825bc03c32
ngoctlm24itb-alt <ngoctlm.24itb@vku.udn.vn>
2026-01-28 08:40:53 +0700

    Initial commit

---
+++ app/src/main/java/com/example/card/CardActivity.kt
@@ -0,0 +1,11 @@
+package com.example.card
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class CardActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_card) // Kết nối với giao diện ở trên
+    }
+}
